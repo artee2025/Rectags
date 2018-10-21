fc5e9bb34a80f941b2b63a87e31c9c6479c893ea
Artee <artee2025@gmail.com>
2018-10-21 14:47:38 +0700

    edit

--- rectags.php
+++ rectags.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              http://artee.io/
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           RecTags
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/rectags-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       RecTags
+ * Plugin URI:        http://artee.io/pr/rectags/
+ * Description:       Tab cloud in the form of rectangles
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Artee
+ * Author URI:        http://artee.io/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       rectags
